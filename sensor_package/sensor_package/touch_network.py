4194c653944f54263ec45087f29a2d62a6bca65d
Michalis Papadakis <michael.d.papadakis@gmail.com>
2025-12-05 18:51:09 +0100

    touch network for newest sensor (mean)

--- sensor_package/sensor_package/touch_network.py
+++ sensor_package/sensor_package/touch_network.py
@@ -28,7 +28,7 @@ class MinimalSubscriber(Node):
         self.buffer = []
         self.model = keras.models.load_model(f"/home/papaveneti/ros_ws/src/sensor_package/sensor_package/config/model_position.keras")
         self.x_scaler = joblib.load(f"/home/papaveneti/ros_ws/src/sensor_package/sensor_package/config/x_scaler_position.save")
-        self.time_steps = 3
+        self.time_steps = 15
 
 
     def listener_callback(self, msg):
@@ -43,7 +43,11 @@ class MinimalSubscriber(Node):
                 self.buffer.pop(0)
                 #self.buffer.pop(-1)
             if len(self.buffer) == self.time_steps:
-                stacked = np.array(self.buffer)  # shape = (time_steps, 12)
+                stacked_ = np.array(self.buffer)  # shape = (time_steps, 12)
+                Dstacked = stacked_ - np.mean(stacked, axis=0)
+                stacked = np.concatenate([stacked_, Dstacked], axis=1)
+
+
                 X_scaled = self.x_scaler.transform(stacked.reshape(-1,12))
                 X_scaled = X_scaled.reshape(1, self.time_steps, 12)
                 y_pred = self.model.predict(X_scaled)
